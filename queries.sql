3a110b269602e36cff485c08da4a1db946c952e3
Melvin Sparkman <93449867+melvinSparkMan@users.noreply.github.com>
2022-03-24 18:16:54 -0500

    Graded Assignment-#4: Final Submission

--- queries.sql
+++ queries.sql
@@ -2,6 +2,16 @@
 
 ## Part 2: Test it with SQL
 
+SELECT NAME FROM employer where location = "Saint Louis City"
+
 ## Part 3: Test it with SQL
 
+DROP TABLE job
+
 ## Part 4: Test it with SQL
+
+	SELECT name, description
+FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY NAME ASC;
